66374c2f4be48a0f882eba60bb1282a581f1f5d4
AVCADO <devavcado@gmail.com>
2020-10-24 02:34:25 +0000

    I'm not sure that boot.c is a bootloader tho

--- kernel/boot.c
+++ kernel/boot.c
@@ -1,11 +1,18 @@
+/* Bootloader for Sleepy OS.
+ * I am not 100% sure it's a bootloader
+ * though */
+
 #include <efi.h>
 #include <efilib.h>
  
+
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable){
     EFI_STATUS Status;
     EFI_INPUT_KEY Key;
      ST = SystemTable;
     Status = ST->ConOut->OutputString(ST->ConOut, L"Welcome to SleepyOS\n\r");
+    Status = ST->ConOut->OutputString(ST->ConOut, L"Please choose an option below.\n\r"); // We can rewrite the status
+    // variable twice to print out more information
     if (EFI_ERROR(Status))
         return Status;
     Status = ST->ConIn->Reset(ST->ConIn, FALSE);
