664cfb8233734bf1b30c4ad66dcb1edc1eae3da7
AVCADO <devavcado@gmail.com>
2020-10-27 19:56:28 +0000

    cleaned

--- bin/obj/hal/Kernel.cpp.o.d
+++
@@ -1,7 +0,0 @@
-/home/nick/Programming/SleepyOS/bin/obj/Kernel.cpp.o: \
-  /home/nick/Programming/SleepyOS/src/Kernel/Kernel.cpp \
-  /home/nick/Programming/SleepyOS/src/HAL/Headers/terminal.h \
-  /usr/lib/clang/10.0.1/include/stddef.h \
-  /usr/lib/clang/10.0.1/include/__stddef_max_align_t.h \
-  /usr/lib/clang/10.0.1/include/stdint.h /usr/include/stdint.h \
-  /home/nick/Programming/SleepyOS/src/Utilities/macro.h
