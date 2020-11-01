47e4bacb2469a9da855caf46cea0e1f5412339d7
AVCADO <devavcado@gmail.com>
2020-10-31 19:36:42 -0500

    fixing everything

--- src/Kernel/genimage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-mkdir -p ./iso/boot/grub
-cp bin/sleepy.bin ./iso/boot/sleepy.bin
-cp src/Kernel/grub.cfg ./iso/boot/grub/grub.cfg
-
-grub-mkrescue -o ./sleepy.iso ./iso || exit
--- src/Utilities/macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef SLEEPY_MACRO_H
-#define SLEEPY_MACRO_H
-
-#define DO(x) for (int a = 1; a <= x; a++) // For loop kindof
-
-#endif
